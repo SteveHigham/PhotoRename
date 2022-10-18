fb07115e64a3af753fb72ff97332e2b5adf240ca
Steve Higham (MSI Ubuntu) <steve@sjlt.co.uk>
2022-10-18 03:47:45 +0100

    Obtaining sorted list of file numbers.

--- src/main/groovy/Main.groovy
+++ src/main/groovy/Main.groovy
@@ -1,3 +1,24 @@
-static void main(String[] args) {
-  println "Hello world!"
+static void main (String[] args)
+{
+  def folder = new File ('/home/steve/Pictures')
+  def fileNumbers = []
+
+  // Collect the embedded numbers for each file
+  folder.eachFileMatch (~/IMG_\d+\.JPG/) { file ->
+    fileNumbers.add (parseFileNumber (file.name))
+  }
+  fileNumbers = fileNumbers.sort ()
+
+  // Iterate through files in numeric sequence - the order the pictures were generated
+  fileNumbers.each { num ->
+    println ("IMG_${num}.JPG")
+  }
 }
+
+static int parseFileNumber (String fileName)
+{
+  def result = (fileName =~ /\d+/).findAll () [0]
+  return result as int
+}
+
+// End of Main.groovy
