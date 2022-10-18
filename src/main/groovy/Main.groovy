051f52ae3a71a7e64cf168065fe089763fa42ce4
Steve Higham (MSI Ubuntu) <steve@sjlt.co.uk>
2022-10-18 20:16:47 +0100

    Working code.

--- src/main/groovy/Main.groovy
+++ src/main/groovy/Main.groovy
@@ -2,6 +2,7 @@ static void main (String[] args)
 {
   def folder = new File ('/home/steve/Pictures')
   def fileNumbers = []
+  def dry = false
 
   // Collect the embedded numbers for each file
   folder.eachFileMatch (~/IMG_\d+\.JPG/) { file ->
@@ -10,8 +11,23 @@ static void main (String[] args)
   fileNumbers = fileNumbers.sort ()
 
   // Iterate through files in numeric sequence - the order the pictures were generated
-  fileNumbers.each { num ->
-    println ("IMG_${num}.JPG")
+  def date = ''
+  def num = 10
+  fileNumbers.each { fileNum ->
+    def file = new File (folder, "IMG_${fileNum}.JPG")
+    assert (file.exists ())
+    def fileDate = new Date (file.lastModified ()).format ('yyyy-MM-dd')
+    if (date == fileDate) { num++ } else {
+      date = fileDate
+      num = 1
+    }
+    def numString = num.toString ().padLeft (4, '0')
+    def newName = "${date}_${numString}.jpg"
+    if (dry) {
+      println("Rename $file.name to $newName")
+    } else {
+      file.renameTo (new File (folder, newName))
+    }
   }
 }
 
